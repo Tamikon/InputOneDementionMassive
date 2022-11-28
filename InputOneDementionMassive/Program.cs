8f304e30a48d0a8321d38a1d95aa43270b7af50b
Станислав Бутов <butov.s-butov@yandex.ru>
2022-11-28 11:06:53 +0300

    Добавьте файлы проекта.

---
+++ InputOneDementionMassive/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOneDementionMassive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 5, 8, 1, 9, 78 };
+            foreach (var item in mas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
