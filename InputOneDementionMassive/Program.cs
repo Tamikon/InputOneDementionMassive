5e764918b6d165c9d254df4d1e8b404c28860e64
Станислав Бутов <butov.s-butov@yandex.ru>
2022-11-28 11:20:18 +0300

    Ввод элементов массива в столбик.

--- InputOneDementionMassive/Program.cs
+++ InputOneDementionMassive/Program.cs
@@ -10,7 +10,16 @@ namespace InputOneDementionMassive
     {
         static void Main(string[] args)
         {
+            // ввод элементов массива в столбик
+
+            int n = Convert.ToInt32(Console.ReadLine());
             int[] mas = { 5, 8, 1, 9, 78 };
+
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
             foreach (var item in mas)
             {
                 Console.WriteLine(item);
